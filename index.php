180b6ad637b7f6c29358fdbe451300af6dff0fd9
sumitsaini78 <55628481+sumitsaini78@users.noreply.github.com>
2025-12-09 19:08:01 +0530

    kk

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ include('includes/connect.php');
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
-
+  
 <body>
     <!-- navbar -->
     <div class="container-fluid ">
